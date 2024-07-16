50792e787207fe4b19dfacce690adeda89d65c9c
Evgeny Kurnevsky <kurnevsky@gmail.com>
2024-07-16 16:08:46 +0200

    Add property test.

--- build.sbt
+++ build.sbt
@@ -5,8 +5,10 @@ ThisBuild / semanticdbVersion := scalafixSemanticdb.revision
 
 val cats = "org.typelevel" %% "cats-core" % "2.12.0"
 val munit = "org.scalameta" %% "munit" % "1.0.0" % Test
+val scalacheck = "org.scalacheck" %% "scalacheck" % "1.18.0" % Test
+val munitScalacheck = "org.scalameta" %% "munit-scalacheck" % "1.0.0" % Test
 
 val field = project
   .settings(
-    libraryDependencies ++= Seq(cats, munit),
+    libraryDependencies ++= Seq(cats, munit, scalacheck, munitScalacheck),
   )
---
+++ field/src/test/scala/points/field/RandomGameFieldSpec.scala
@@ -0,0 +1,27 @@
+package points.field
+
+import org.scalacheck.*
+import org.scalacheck.Prop.*
+
+import scala.util.Random
+
+class RandomGameFieldSpec extends munit.ScalaCheckSuite:
+  val minSize = 3
+  val maxSize = 50
+  val lengthGen = Gen.choose(minSize, maxSize)
+  val seedGen = Arbitrary.arbitrary[Int]
+
+  property("random game") {
+    Prop.forAll(lengthGen, lengthGen, seedGen) { (width: Int, height: Int, seed: Int) =>
+      val random = new Random(seed)
+      val moves = random.shuffle((0 until width * height).toVector).map(idx => Pos(idx % width, idx / width))
+      val field = Field(width, height)
+      val finalField = moves.foldLeft(field)((acc, pos) => acc.putPoint(pos).getOrElse(acc))
+      all(
+        (finalField.scoreRed >= 0) :| "red score should be non-negative",
+        (finalField.scoreBlack >= 0) :| "black score should be non-negative",
+        ((field.scoreRed - field.scoreBlack).abs < width * height / 2) :| "score difference should be less than number of player moves",
+        (field.scoreRed + field.scoreBlack <= (width - 2) * (height - 2)) :| "full score should be less than field size",
+      )
+    }
+  }
